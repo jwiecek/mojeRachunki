be6293ef2b8feed325eddbb690e9851830200c5a
jwiecek <47353580+jwiecek@users.noreply.github.com>
2019-05-17 09:22:56 +0200

    Delete database.providers.ts

--- src/database/database.providers.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from 'mongoose';
-
-export const databaseProviders = [
-  {
-    provide: 'DbConnectionToken',
-    useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect('mongodb://Mandi:Neokolk!23@ds217976.mlab.com:17976/my-bills'),
-  },
-];
